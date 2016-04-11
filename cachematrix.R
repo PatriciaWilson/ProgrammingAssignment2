487ba5a270921672546754b3f9023a2845ed0a5c
Patricia Wilson <patricia.a.wilson.84106@gmail.com>
2016-04-10 19:24:09 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##Step 1 Create makeCasheMatrix function
+#Make this following function
+#I am both cashing the inverse of the data and displaying it.
+makeCasheMatrix <- function(x){
+  InvX <<- solve(x)
+  return(InvX)
 }
 
+#Create Data to test function
 
-## Write a short comment describing this function
+TestData <- matrix(c(2, 3, 5, 8),
+                   nrow = 2, byrow = TRUE)
+#Store inverse matrix cashe for testing.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+makeCasheMatrix(TestData)
+
+#confirming that above function works.
+
+ConfirmMatrix <- TestData%*%InvX
+
+# Round as R displays
+round(ConfirmMatrix, digits = 2)
+
+## Part 2 Cache the Inverse of a Matrix
+
+CasheSolve <- function(x = InvX){
+  InvInvX <<- solve(x)
+  return(InvInvX)}
+
+#Testing this function out
+CasheSolve(InvX)
+
+#Additional check that matrix has been returned to original 
+# form by inverting the inverted matrix
+
+mean(round(InvInvX - TestData, digits = 10))==0
